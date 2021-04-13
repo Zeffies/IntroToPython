254d3829ec1fd69fb62da9b45e6a20ca431b9660
Zeffies <zeffiesmc@gmail.com>
2021-04-13 17:17:54 -0400

    Posts both the alphabet and their numbers with no spaces. No idea dude. Just including for archival's sake.

---
+++ alphabetnumbered.py
@@ -0,0 +1,7 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for x in range(0,26):
+    print(x+1, end="")
+print('\n')
+for x in range(0,26):
+    print(alphabet[x], end="")
