74df809a77334a603defe9cd87238ed73ae63ad2
Zeffies <zeffiesmc@gmail.com>
2021-04-13 17:18:45 -0400

    Another car class program, practicing classes. Also intros the dir() function.

---
+++ car.py
@@ -0,0 +1,19 @@
+class Car():
+    
+    def __init__(self, make, model, year, num_doors=None, owner=None):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.num_doors = num_doors
+        self.owner = owner
+        
+    def describe_car(self):
+        print("Make: %s" % self.make)
+        print("Model: %s" % self.model)
+        print("Year: %d" % self.year)
+        if self.num_doors:
+            print("Number of Doors: %d" % self.num_doors)
+        if self.owner:
+            print("Owner: %s" % self.owner)
+            
+print(dir(Car))
