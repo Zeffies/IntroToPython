0ced9621484f6687403e9feee25ea6ef40d23552
Zeffies <zeffiesmc@gmail.com>
2021-04-13 14:41:17 -0400

    Program to practice slicing strings.

---
+++ AlphabetSlices.py
@@ -0,0 +1,8 @@
+#Store the first ten letters of the alphabet in a list.
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+#Use a slice to print out the first three letters of the alphabet.
+print(alphabet[0:3])
+#Use a slice to print out any three letters from the middle of your list.
+print(alphabet[3:6])
+#Use a slice to print out the letters from any point in the middle of your list, to the end.
+print(alphabet[3:])
