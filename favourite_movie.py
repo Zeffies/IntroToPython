fc80387597d65b128dc2e4a6cdc336cdbe520147
Zeffies <zeffiesmc@gmail.com>
2021-04-13 19:24:43 -0400

    Function with default keyword argument

---
+++ favourite_movie.py
@@ -0,0 +1,6 @@
+def favourite_movie(movie='the princess bride'):
+    print("My favourite movie is %s!" % movie.title())
+    
+favourite_movie('the matrix')
+favourite_movie('amadeus')
+favourite_movie()
