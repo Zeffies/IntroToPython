fccaad48289a9f3d5ccda52cb712741362814042
Zeffies <zeffiesmc@gmail.com>
2021-04-14 09:59:24 -0400

    learning .append()

---
+++ StartingFromEmpty.py
@@ -0,0 +1,12 @@
+# Create the list you ended up with in Working List, but this time start your file with an empty list and fill it up using append() statements.
+careers = []
+careers.append('Dog Walker')
+careers.append('Programmer')
+careers.append('Truck Driver')
+careers.append('Construction Worker')
+careers.append('Teacher')
+careers.append('Clerk')
+# Print a statement that tells us what the first career you thought of was.
+print('The first career I thought of was ' + careers[1] + '.')
+# Print a statement that tells us what the last career you thought of was
+print('The last career I thought of was ' + careers[0] + '.')
