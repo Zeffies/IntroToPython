e14816b1527ab75c16e8a53c1de83bf6c8290500
Zeffies <zeffiesmc@gmail.com>
2021-04-14 09:51:52 -0400

    function practice

---
+++ ReturnCalculator.py
@@ -0,0 +1,10 @@
+# Write a function that takes in two numbers, and adds them together.
+def add_calc(num1, num2):
+     calced = num1 + num2
+     print('%d + %d = %d' % (num1, num2, calced))
+# Make your function print out a sentence showing the two numbers, 
+# and the result.
+# Call your function with three different sets of numbers.
+add_calc(30, 70)
+add_calc(21, 23)
+add_calc(315, 73)
