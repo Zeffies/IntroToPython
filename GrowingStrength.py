0fbc193acaf0399691b6eb923801d05854ccd840
Zeffies <zeffiesmc@gmail.com>
2021-04-13 21:34:52 -0400

    while loop intro

---
+++ GrowingStrength.py
@@ -0,0 +1,19 @@
+# Make a variable called strength, and set its initial value to 5.
+strength = 5
+# Print a message reporting the player's strength.
+print('Your current strength is %d.\n' % strength)
+# Set up a while loop that runs until the player's 
+#   strength increases to a value such as 10.
+while strength <= 15:
+    print('Your current strength is %d.\n' % strength)
+    strength = strength + 1
+print("You've grown too strong! You've been moved to a new level of the game.")
+# Inside the while loop, print a message 
+#   that reports the player's current strength.
+# Inside the while loop, write a statement that increases the player's strength.
+# Outside the while loop, print a message reporting 
+#   that the player has grown too strong, 
+#   and that they have moved up to a new level of the game.
+# Bonus: Play around with different cutoff levels for the value of strength, 
+#   and play around with different ways to 
+#   increase the strength value within the while loop.
