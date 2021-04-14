e578373562d5decdd61471a2d4058ab2cca4cb1a
Zeffies <zeffiesmc@gmail.com>
2021-04-14 10:16:54 -0400

    function practice

---
+++ world_languages.py
@@ -0,0 +1,6 @@
+def world_languages(country, language):
+    print("%s primarily speaks %s." % (country, language))
+    
+world_languages(language='English', country='England')
+world_languages('Germany', 'German')
+world_languages('France', 'French')
