46519b6bf698a2b479fe6a9b72c6ea1632d428ef
Zeffies <zeffiesmc@gmail.com>
2021-04-14 09:46:30 -0400

    title bar example for text only programs

---
+++ PersistantTitleBar.py
@@ -0,0 +1,8 @@
+# Greeter is a terminal application that greets old friends warmly,
+#   and remembers new friends.
+
+# Display a title bar.
+print("\t**********************************************")
+print("\t***  Greeter - Hello old and new friends!  ***")
+print("\t**********************************************")
+
