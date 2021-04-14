7c87c8c1d678a41c0083fa49a98dab4ac6503549
Zeffies <zeffiesmc@gmail.com>
2021-04-13 22:07:42 -0400

    len()

---
+++ ListLengths.py
@@ -0,0 +1,4 @@
+numbers = [51, 73, 1, 40, 103]
+careers = ['programmer', 'truck driver', 'construction worker', 'teacher']
+print('The list of numbers has ' + str(len(numbers)) + ' things in it.')
+print('The list of careers has ' + str(len(careers)) + ' things in it.')
