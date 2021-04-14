81effae16afde95b9d91a87b0fab6aacc499b83d
Zeffies <zeffiesmc@gmail.com>
2021-04-14 09:58:55 -0400

    function practice, specifying the argument when calling function

---
+++ sports_teams.py
@@ -0,0 +1,6 @@
+def sports_teams(city, team):
+    print("%s: %s" % (city, team))
+
+sports_teams("Indianapolis", "Colts")
+sports_teams(team='Cowboys', city='Dallas')
+sports_teams("Boston", "Red Sox")
