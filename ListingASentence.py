fdf9adeb466600cc55d4a45e035b0f8a8c8f2584
Zeffies <zeffiesmc@gmail.com>
2021-04-13 22:08:16 -0400

    for loop, print every letter in a sentence on a new line

---
+++ ListingASentence.py
@@ -0,0 +1,5 @@
+#Store a single sentence in a variable. 
+sentence = 'This is a sentence.'
+#Use a for loop to print each character from your sentence on a separate line.
+for character in sentence:
+	print(character)
