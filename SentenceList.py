4ed07a6366f0bbbddac9322f50a843cdf2f6f017
Zeffies <zeffiesmc@gmail.com>
2021-04-14 09:56:46 -0400

    .split() method

---
+++ SentenceList.py
@@ -0,0 +1,7 @@
+#Store a single sentence in a variable. 
+sentence = 'This is a sentence.'
+#Create a list from your sentence.
+sentence_list = sentence.split(' ')
+#Print your raw list (don't use a loop, just print the list).
+print(sentence_list)
+
