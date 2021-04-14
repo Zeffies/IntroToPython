18dd266f5262bb5cfbe16e31eb940b4f9ff8e545
Zeffies <zeffiesmc@gmail.com>
2021-04-14 10:00:48 -0400

    learning booleans and comparisons

---
+++ TrueAndFalse.py
@@ -0,0 +1,20 @@
+print('1 ' + str(3 == 1))
+print('eric' == 27)
+print('a' == 1)
+print(3 > 2)
+print(100 <= 109)
+print(30 != 29)
+print('b' == 'B')
+print((30+30) == 60)
+print(80 >= 100)
+print(5 == 5)
+# False
+# False
+# False
+# True
+# True
+# True
+# False
+# True
+# False
+# True
